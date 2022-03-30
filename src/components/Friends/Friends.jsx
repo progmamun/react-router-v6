83ab6050f652a8b5ebca85c4790840167906cff0
progmamun <mamun333089@gmail.com>
2022-03-30 16:11:32 +0600

    display-users-dynamic-link-use-navigate

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Friend from '../Friend/Friend';
 
 const Friends = () => {
+  const [friends, setFriends] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users/')
+      .then(res => res.json())
+      .then(data => setFriends(data));
+  }, []);
   return (
     <div>
-      <h1>friends components</h1>
+      <h1>Hello Friends.</h1>
+      {friends.map(friends => (
+        <Friend key={friends.id}></Friend>
+      ))}
     </div>
   );
 };
