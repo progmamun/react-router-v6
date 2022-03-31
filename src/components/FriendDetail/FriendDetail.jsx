74d16b3fc0532bfffa3c54c88013aa8f9ccc9d61
progmamun <mamun333089@gmail.com>
2022-03-31 07:56:28 +0600

    route-parameter-load-data

--- src/components/FriendDetail/FriendDetail.jsx
+++ src/components/FriendDetail/FriendDetail.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const FriendDetail = () => {
   // const params = useParams();
-  const { detail } = useParams();
+  const { friendId } = useParams();
+  const [friend, setFriend] = useState({});
+
+  useEffect(() => {
+    const url = `https://jsonplaceholder.typicode.com/users/${friendId}`;
+    fetch(url)
+      .then(res => res.json())
+      .then(data => setFriend(data));
+  }, []);
+
   return (
     <div>
-      <h2>FriendDetail: {detail}</h2>
+      <h2>FriendDetail: {friendId}</h2>
+      <h6>Website: {friend.website}</h6>
+      <p>
+        <small>city: {friend.address?.city}</small>
+      </p>
+      <p>
+        <small>Lat: {friend.address?.geo?.lat}</small>
+      </p>
     </div>
   );
 };
